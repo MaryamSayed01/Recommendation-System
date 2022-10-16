5e989bb5ef4ddee14dfbcbfeac6111e3c2e4a4ce
Maryam Sayed <78811400+MaryamSayed01@users.noreply.github.com>
2022-10-16 16:31:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,10 +9,5 @@ echo"\
 [server]\n\
 headless=true\n\
 enableCORS=false\n\
-<<<<<<< HEAD:setup.sh.txt
 port= $PORT\n\
 "> ~/ .streamlit/config.toml
-=======
-\n\
-"> ~/ .streamlit/config.toml
->>>>>>> edceea0e14697133566b521feb6879d76872f08f:setup.sh
